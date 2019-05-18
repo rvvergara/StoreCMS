3e84725934c402fc5f5f93ed6611e60b46cf023c
inhaq <p126013@nu.edu.pk>
2019-05-18 23:27:22 +0500

    minor fixes and stickler add

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -46,8 +46,8 @@ class BooksForm extends Component {
       <form className="formBook">
         <h1 className="addBook">ADD NEW BOOK</h1>
         <input onChange={e => this.handleChange('title',e.target.value)} value={this.state.title} />
-        <select onChange={e => this.handleChange('category',e.target.value)}>
-        <option disabled selected>Select</option>
+        <select defaultValue={'DEFAULT'} onChange={e => this.handleChange('category',e.target.value)}>
+        <option value="DEFAULT" disabled>Select</option>
           {options}
         </select>
         <button onClick={this.handleSubmit.bind(this)}>Submit</button>
